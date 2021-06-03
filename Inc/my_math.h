560dd3d36908e172bb120c8df15462ef72c90f62
florian-devin <florian.devin13@gmail.com>
2021-06-03 15:55:59 +0200

    ajout de fct map()

---
+++ Inc/my_math.h
@@ -0,0 +1,20 @@
+/**
+ * @brief like arduino float map()
+ * @param val input value
+ * @param I_Min min input val
+ * @param I_Max max output val
+ * @param O_Min min output val
+ * @param O_Max max output val
+ * @retval (float) output val
+ */
+float mapf(float val, float I_Min, float I_Max, float O_Min, float O_Max);
+/**
+ * @brief like arduino int map()
+ * @param val input value
+ * @param I_Min min input val
+ * @param I_Max max output val
+ * @param O_Min min output val
+ * @param O_Max max output val
+ * @retval (float) output val
+ */
+int map(int val, int I_Min, int I_Max, int O_Min, int O_Max);
