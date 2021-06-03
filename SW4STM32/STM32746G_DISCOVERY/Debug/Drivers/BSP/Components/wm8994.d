f3a54c9c30675a1eec0876ead67a847dbd1cde29
florian-devin <56131838+florian-devin@users.noreply.github.com>
2021-06-03 13:45:18 +0200

    Delete dir debug

--- SW4STM32/STM32746G_DISCOVERY/Debug/Drivers/BSP/Components/wm8994.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/wm8994.o: \
- C:/Users/thoma/STM32CubeIDE/workspace_1.6.1/BSP/Drivers/BSP/Components/wm8994/wm8994.c \
- C:/Users/thoma/STM32CubeIDE/workspace_1.6.1/BSP/Drivers/BSP/Components/wm8994/wm8994.h \
- c:\users\thoma\stm32cubeide\workspace_1.6.1\bsp\drivers\bsp\components\common\audio.h
-
-C:/Users/thoma/STM32CubeIDE/workspace_1.6.1/BSP/Drivers/BSP/Components/wm8994/wm8994.h:
-
-c:\users\thoma\stm32cubeide\workspace_1.6.1\bsp\drivers\bsp\components\common\audio.h:
